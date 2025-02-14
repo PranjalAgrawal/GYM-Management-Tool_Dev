adf35e6e4f1e41993343bc6a5177be8cf2532ab5
Saurabh880 <backtohome924@gmal.com>
2025-02-14 19:28:39 +0530

    initial commit

---
+++ Gym_management_tool/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Gym_management_tool.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym_management_tool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
